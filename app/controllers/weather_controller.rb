22994662b4f40c0afb1e75fa602250f8b02ca533
kaoru-528 <origami.kaoru@gmail.com>
2024-11-26 12:27:31 +0900

    feat:平均降水量の表示

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,13 @@
 class WeatherController < ApplicationController
   def index
+    # region_code = get_region_code(city_name)
+    region_code = '130010' # 八王子市のregion_code
+
     source_1_precipitation = WeatherService.fetch_precipitation_from_source_1
     source_2_precipitation = WeatherService.fetch_precipitation_from_source_2
+    source_3_precipitation = WeatherService.fetch_precipitation_from_source_3
 
-    precipitations = [source_1_precipitation, source_2_precipitation].compact
+    precipitations = [source_1_precipitation, source_2_precipitation, source_3_precipitation].compact
     @average_precipitation = precipitations.sum / precipitations.size if precipitations.any?
   end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -4,6 +4,7 @@ require 'json'
 class WeatherService
   BASE_URL = 'https://weather.tsukumijima.net/api/forecast/city/'.freeze
   HACHIOJI_ID = '130010'.freeze # 八王子市のID
+  JMA_API_URL = 'https://www.jma.go.jp/bosai/forecast/data/forecast/130010.json'.freeze # 八王子市のコード
 
   def self.fetch_precipitation_from_source_1
     url = URI.parse("#{BASE_URL}#{HACHIOJI_ID}")
@@ -12,8 +13,7 @@ class WeatherService
 
     if weather_data['forecasts'] && weather_data['forecasts'][0]['chanceOfRain']
       chance_of_rain = weather_data['forecasts'][0]['chanceOfRain']
-      
-      chance_of_rain['T00_06'].to_i if chance_of_rain['T00_06']
+      chance_of_rain['T18_24'].to_i if chance_of_rain['T18_24']
     else
       nil
     end
@@ -21,8 +21,6 @@ class WeatherService
     Rails.logger.error("Error fetching precipitation: #{e.message}")
     nil
   end
-
-  JMA_API_URL = 'https://www.jma.go.jp/bosai/forecast/data/forecast/130010.json'.freeze # 八王子市のコード
 
   def self.fetch_precipitation_from_source_2
     uri = URI(JMA_API_URL)
@@ -38,6 +36,36 @@ class WeatherService
     else
       nil
     end
+  rescue StandardError => e
+    Rails.logger.error("Error fetching precipitation from JMA API: #{e.message}")
+    nil
+  end
+
+  def self.fetch_precipitation_from_source_3
+  # URIを設定
+  uri = URI('https://map.yahooapis.jp/weather/V1/place?coordinates=139.3667929,35.6613427&appid=dj00aiZpPUpKU2Nkd2Zxb2x1QiZzPWNvbnN1bWVyc2VjcmV0Jng9MTU-')
+
+  # リクエストを送信
+  res = Net::HTTP.get_response(uri)
+
+  if res.is_a?(Net::HTTPSuccess)
+    # レスポンスボディをXMLとしてパース
+    xml = Nokogiri::XML(res.body)
+
+    # 名前空間を登録
+    namespace = { 'ns' => 'http://olp.yahooapis.jp/ydf/1.0' }
+
+    # Rainfall値を抽出し、数値に変換
+    rainfall_values = xml.xpath('//ns:Rainfall', namespace).map(&:text).map(&:to_f)
+
+    # 平均値を計算
+    average_rainfall = (rainfall_values.sum / rainfall_values.size) * 100
+
+    # 結果を表示（平均降水量）
+    average_rainfall.round(2)
+  else
+    nil
+  end
   rescue StandardError => e
     Rails.logger.error("Error fetching precipitation from JMA API: #{e.message}")
     nil
